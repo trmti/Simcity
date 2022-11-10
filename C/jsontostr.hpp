978b889b2bfe051769fd629391641b0e844bf328
RintaroAkashi <blbtijsjoubsp2125@outlook.jp>
2022-11-11 00:16:43 +0900

    死にそう

---
+++ C/jsontostr.hpp
@@ -0,0 +1,4 @@
+#include <string>
+using namespace std;
+
+string conv_json_to_str(char* f_path);
--- C/test2.cpp
+++ C/test2.cpp
@@ -8,10 +8,16 @@ using namespace std;
 #include <jsoncpp/json/json.h>
 #include <jsoncpp/json/writer.h>
 #include <jsoncpp/json/reader.h>
+#include "./jsontostr.hpp"
 
 int main()
 {
+    char path[20] = "./json/persons.json";
+    string body = conv_json_to_str(path);
+
+    /*
     ifstream ifs("./json/persons.json");
+    
     string line;
     string body = "";
 
@@ -23,13 +29,12 @@ int main()
 
     while(getline(ifs, line))
     {
-        //cout << "#" << line << endl;
         body.append(line);
     }
-    //cout << body << endl;
+    */
     
     Json::Reader reader;
     Json::Value value;
     reader.parse(body, value);
-    cout << value[0]["id"] << endl;
+    cout << value[1]["home"][0] << endl;
 }
